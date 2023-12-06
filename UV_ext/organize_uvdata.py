13ef3fe3fdc73f70b24756be42a79ed8d6beb90f
Justin Pierel <jpierel@stsci.edu>
2023-12-05 21:07:34 -0500

    fixed spec and lcs

--- UV_ext/organize_uvdata.py
+++ UV_ext/organize_uvdata.py
@@ -3,16 +3,61 @@ import sys,glob,os
 from astropy.table import Table
 import shutil
 import numpy as np
-#import sncosmo
-#import matplotlib.pyplot as plt
-
+import sncosmo
+import astropy.units as u
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter('ignore')
 filt_remove = ['CfA-J/J','CfA-J/j','CfA-K/K','CfA-K/k','CfA-H/H','CfA-H/h','CSP-Y_RC/C',
                    'CSP-J_RC1/E','CSP-J_RC2/F','CSP-H_RC/G','WFC3_IR','UKIRT']
 global_swap = {'ZTF-G/X':'ZTF-g','ZTF-g/X':'ZTF-g','ZTF-G':'ZTF-g','ZTF-R/Y':'ZTF-r','ZTF-R':'ZTF-r','ZTF-r/Y':'ZTF-r',
                    'ZTF-I':'ZTF-i','cyan-ATLAS':'ATLAS-cyan/c','orange-ATLAS':'ATLAS-orange/o','ATLAS-orange':'ATLAS-orange/o',
                    'ATLAS-o':'ATLAS-orange/o','ATLAS-orange/a':'ATLAS-orange/o'}
 
-def add_spectrum_to_file(fname,spec_name,filt_dict=None):
+
+def register_sncosmo_bandpass(filtwave,filttrans,filtname,unit=u.angstrom):
+        band = sncosmo.Bandpass(filtwave,
+                                filttrans,
+                                wave_unit=u.angstrom,name=filtname)
+        sncosmo.register(band, force=True)
+        
+def get_filter_info(kcor_filename,lightcurve,n):
+        offset_lightcurve = Table(rows=None,names=['mjd','band','flux','fluxerr','zp','zpsys'],
+                     dtype=('f8','S1','f8','f8','f8','U5')
+                     )
+        hdu = fits.open(kcor_filename)
+        zpoff_tab = Table.read(kcor_filename,format='fits')
+        zpoff = {row['Filter Name'].strip():{col:row[col] for col in [x for x in zpoff_tab.colnames if x!='Filter Name']}\
+                     for row in zpoff_tab}
+        snsed = hdu[2].data
+        temp_filtertrans = hdu[5].data
+        filtertrans = {temp_filtertrans.names[i]:np.array([x[i] for x in temp_filtertrans]) for i in range(len(temp_filtertrans.names))}
+        primarysed = hdu[6].data
+        hdu.close()
+        for i in range(len(lightcurve)):
+            m = lightcurve['MJD'][i]
+            flt = lightcurve['FLT'][i]
+            try:
+                sys = zpoff[flt]['Primary Name'].strip()
+            except:
+                continue
+            flx = lightcurve['FLUX'][i]
+            flxe = lightcurve['FLUXERR'][i]
+            offset_lightcurve.add_row((m,flt,flx*10**(0.4*zpoff[flt]['Primary Mag']),
+                              flxe*10**(0.4*zpoff[flt]['Primary Mag']),
+                              27.5,sys))
+        
+        filters = np.unique(offset_lightcurve['band'])
+
+        for band in filters:
+            register_sncosmo_bandpass(filtertrans['wavelength (A)'],
+                                      filtertrans[band],
+                                      band)
+        return offset_lightcurve
+    
+def add_spectrum_to_file(fname,spec_name,snname,filt_dict=None):
+
     with open(fname,'r') as f:
         lines = f.readlines()
     with open(fname,'r') as f:
@@ -21,17 +66,39 @@ def add_spectrum_to_file(fname,spec_name,filt_dict=None):
     if 'SPECTRUM_END:' not in dat:
         lines.append('# =============================================\n')
         lines.append('# =============================================\n')
-        lines.append('NSPECTRA: 1\n')
+        lines.append('NSPECTRA: 0\n')
         lines.append('\n')
         lines.append('NVAR_SPEC: 5\n')
         lines.append('VARNAMES_SPEC: LAMMIN LAMMAX  FLAM  FLAMERR SPECFLAG\n')
         lines.append('\n')
-        lines.append('SPECTRUM_ID: 1\n')
         nspec = 0
+    
     if True:
+        tab = {'MJD':[],'FLT':[],'FLUX':[],'FLUXERR':[]}
         keep = []
-        print(filt_dict)
+        found_survey = False
+        found_z = False
+
+        if 'MWEBV:' in dat or ('RA:' in dat and 'DEC:' in dat):
+            found_mw_or_RA = True
+        else:
+            print('NO MWEBV OR RA/DEC IN ',fname)
+        lines = list(np.append(['SNID: %s\n'%snname],lines))
         for i,line in enumerate(lines):
+            if line.startswith('VARLIST:'):
+                varlist = line.split()
+                if 'FIELD' not in varlist:
+                    lines[i] = lines[i].replace('FLT ','FLT FIELD ')
+            if 'SURVEY:' in line:
+                lines[i] = lines[i].replace(line.split()[-1],'UVEXT')
+                found_survey = True
+            if 'REDSHIFT:' in line:
+                z = float(line.split(':')[1].split()[0])
+                found_z = True
+            elif 'REDSHIFT_HELIO:' in line:
+                z = float(line.split(':')[1].split()[0])
+                found_z = True
+
             if line.startswith('OBS:'):
                 if filt_dict is not None:
                     if line.split()[2] in filt_dict.keys():
@@ -44,14 +111,43 @@ def add_spectrum_to_file(fname,spec_name,filt_dict=None):
                     lines[i] = lines[i].replace('i-ZTF','ZTF-i')
                 if not np.any([x in lines[i].split()[2] for x in filt_remove]):
                     keep.append(i)
+                    
+                    
                 if lines[i].split()[2] in global_swap.keys():
                     lines[i] = lines[i].replace(lines[i].split()[2],global_swap[lines[i].split()[2]])
-            else:
+                if i in keep:
+                    
+                        
+                    tab['MJD'].append(float(lines[i].split()[1]))
+                    tab['FLT'].append(lines[i].split()[2])
+                    if 'MAG' in varlist and float(lines[i].split()[varlist.index('MAG')])>0:
+                        flux = 10**(-.4*(float(lines[i].split()[varlist.index('MAG')])-27.5))
+                        fluxerr = 0.92103 * float(lines[i].split()[varlist.index('MAGERR')]) * flux
+                        tab['FLUX'].append(flux)
+                        tab['FLUXERR'].append(fluxerr)
+                        lines[i] = lines[i].replace(lines[i].split()[varlist.index('FLUXCAL')],'%.4f'%flux)
+                        lines[i] = lines[i].replace(lines[i].split()[varlist.index('FLUXCALERR')],'%.4f'%fluxerr)
+                    elif 'VOID' in line or 'NULL' in line:
+                        tab['FLUX'].append(float(lines[i].split()[4]))
+                        tab['FLUXERR'].append(float(lines[i].split()[5]))
+                    else:
+                        
+                        tab['FLUX'].append(float(lines[i].split()[3]))
+                        tab['FLUXERR'].append(float(lines[i].split()[4]))
+                    if 'FIELD' not in varlist:
+                        lines[i] = lines[i].replace(lines[i].split()[2],lines[i].split()[2]+' VOID ')
+            elif ('SNID' not in line or i==0) or i>0:
                 keep.append(i)
             if line.startswith('NSPECTRA:'):
                 nspec = int(line.split()[-1])
                 lines[i] = line.replace(line.split()[-1],str(int(line.split()[-1])+1))
-                break
+                #break
+        if not found_survey:
+            print('NO SURVEY IN ',fname)
+        if not found_z:
+            print('NO Z IN ',fname)
+
+            
         lines = list(np.array(lines)[np.array(keep)])
         lines.append('\n')
         lines.append('SPECTRUM_ID: %i\n'%(nspec+1))
@@ -68,8 +164,16 @@ def add_spectrum_to_file(fname,spec_name,filt_dict=None):
                                                          row['flux'],row['fluxerr'],1))
     lines.append('SPECTRUM_END:\n')
 
+    
+    tab = Table(tab)
+    tab['zp'] = 27.5
+    tab['zpsys'] = 'ab'
+    #if snname in ['2015F','2013dy','2001eh']:
+    #    return z,tab
     with open(fname,'w') as f:
         f.write(''.join(lines))
+
+    return z,tab
 
 
 _snidlist = ['2021J',
@@ -154,7 +258,8 @@ _snidlist = {'2021J':{'in_saltshaker_format':True,
                       'filter_dict':{}},
              #--
              '2015F':{'in_saltshaker_format':False,
-                      'lc_files':('SN2015F_csp_dr3.dat'),#'SN2015F.dat'),#,'SN2015F_csp_dr3.dat'),
+                      'lc_files':('SN2015F.dat'),#,'SN2015F_csp_dr3.dat'),
+                      #'lc_files':('SN2015F_csp_dr3.dat'),
                       'optical_spec_files':('2015F_spectra.zip'),
                       'uv_spec_files':('2015F_57097.19845327.flm','2015F_57098.74907818.flm','2015F_57100.871648315.flm','2015F_57103.651092755.flm','2015F_57107.026405345.flm',
                                        '2015F_57108.70207646.flm','2015F_57111.928477205.flm','2015F_57114.585270905.flm','2015F_57117.582528130006.flm','2015F_57124.55984294.flm'),
@@ -184,7 +289,8 @@ _snidlist = {'2021J':{'in_saltshaker_format':True,
              #         'uv_spec_files':(),
              #         'filter_dict':{}},
              '2013dy':{'in_saltshaker_format':False,
-                      'lc_files':('SWIFT_2013dy.dat'),#'LOSS_2013dy.dat'),
+                      'lc_files':('LOSS_2013dy.dat'),
+                      #'lc_files':('SWIFT_2013dy.dat'),#'LOSS_2013dy.dat'),
                       'optical_spec_files':(),
                       'uv_spec_files':('sn2013dy_56494.5138120125.flm','sn2013dy_56498.606057245.flm','sn2013dy_56500.329344285.flm','sn2013dy_56502.316740205.flm',
                                        'sn2013dy_56505.578881315.flm','sn2013dy_56509.498418355.flm','sn2013dy_56513.097331234996.flm','sn2013dy_56515.144877535.flm',
@@ -206,6 +312,7 @@ _snidlist = {'2021J':{'in_saltshaker_format':True,
              #         'uv_spec_files':(),
              #         'filter_dict':{}},
              '2001eh':{'in_saltshaker_format':False,
+                      #'lc_files':('SN_2001eh.dat'),
                       'lc_files':('CFA3_4SHOOTER2_2001eh.DAT'),#'SN_2001eh.dat'),#),
                       'optical_spec_files':(),
                       'uv_spec_files':('sn2001eh_52177.759297552504.flm','sn2001eh_52186.42509034666.flm','sn2001eh_52186.42509034667.flm'),
@@ -226,12 +333,12 @@ _snidlist = {'2021J':{'in_saltshaker_format':True,
              #         'uv_spec_files':(),
              #         'filter_dict':{}},
              '2021zfw':{'in_saltshaker_format':False,
-                      'lc_files':('2021zfw.snana.dat'),
+                      'lc_files':('2021zfw_data.snana.txt'),
                       'optical_spec_files':(),
                       'uv_spec_files':('at2021zfw_59498.56909605.flm'),
                       'filter_dict':{}},
-             '2011fe':{'in_saltshaker_format':True,
-                      'lc_files':('2011fe.BayeSN.txt'),
+             '2011fe':{'in_saltshaker_format':False,
+                      'lc_files':('SWIFT_2011fe.dat'),
                       'optical_spec_files':(),
                       'uv_spec_files':('2011fe_55801.211675135.flm','2011fe_55804.30364847.flm','2011fe_55835.29861821.flm','2011fe_55841.349578855.flm',
                                        '2011fe_55855.209197004995.flm','sn2011fe_56191.1212618.flm','sn2011fe_56191.371550165.flm','sn2011fe_56191.70181059834.flm',
@@ -274,17 +381,25 @@ _snidlist = {'2021J':{'in_saltshaker_format':True,
                       'uv_spec_files':('sn2023gft_60078.21623377.flm'),
                       'filter_dict':{}}}
 
-for sn in _snidlist:
-    if sn in ['2015F','2013dy','2001eh']:
+
+filelist = []
+par_file = []
+mjd_file = []
+for n,sn in enumerate(_snidlist):
+    #if sn!='ASASSN14LP':
+    #    continue
+    if sn not in ['2015F','2013dy','2001eh']:
         continue
-    print(sn)
-    print(_snidlist[sn])
+    if sn in ['2021jad','2022eyw']:
+        continue
+    #print(sn)
+    #print(_snidlist[sn])
     try:
     
         new_fname = os.path.join('final_lcs',os.path.basename(_snidlist[sn]['lc_files']))
     except RuntimeError:
         continue
-    if _snidlist[sn]['in_saltshaker_format']:
+    if _snidlist[sn]['in_saltshaker_format'] or (sn in ['2015F','2013dy','2001eh'] and True):
         filt_dict = None
         try:
             shutil.copyfile(_snidlist[sn]['lc_files'],new_fname)
@@ -297,9 +412,10 @@ for sn in _snidlist:
                     _snidlist[sn]['optical_spec_files'] = [_snidlist[sn]['optical_spec_files']]
                 _snidlist[sn]['uv_spec_files'] = [os.path.join('UVspec',x) for x in _snidlist[sn]['uv_spec_files']]
                 _snidlist[sn]['optical_spec_files'] = [os.path.join('OptSpec',x) for x in _snidlist[sn]['optical_spec_files']]
-                new_fname = shutil.copyfile(_snidlist[sn]['lc_files'],new_fname)
-                shutil.copyfile(_snidlist[sn]['lc_files'],new_fname)
-            except RuntimeError:
+                
+                if sn not in ['2015F','2013dy','2001eh']:
+                    shutil.copyfile(_snidlist[sn]['lc_files'],new_fname)
+            except:
                 continue
             
 
@@ -323,6 +439,8 @@ for sn in _snidlist:
             filt_dict = {'a':'CFA3S-U/a','b':'CFA3S-B/b','c':'CFA3S-V/c','d':'CFA3S-R/d','e':'CFA3S-I/e'}
         elif sn=='2021zfw' or sn=='2021dov' or sn=='2023bee':
             filt_dict = {'g':'PS1-g','r':'PS1-r','i':'PS1-i','z':'PS1-z','y':'PS1-y','Y':'ZTF-r','X':'ZTF-g'}
+        elif sn=='2011fe':
+            filt_dict = {'E':'KAIT4-E','M':'nickel2-M','F':'KAIT4-F','N':'nickel2-N','H':'KAIT4-H','P':'nickel2-P','G':'KAIT4-G','O':'nickel2-O','U':'UVOT-U','B':'UVOT-B','V':'UVOT-V','X':'UVOT-X','N':'UVOT-N','W':'UVOT-W'}
         elif sn=='ASASSN14LP':
             temp = Table.read('UVLC/'+_snidlist[sn]['lc_files'],format='ascii')
             temp=temp[temp['Tel']=='CSP']
@@ -379,13 +497,52 @@ for sn in _snidlist:
     #    print(_snidlist[sn])
     #    sys.exit()
     #    continue
+    
     for spec in _snidlist[sn]['uv_spec_files']:
-        print(spec)
         try:
-            add_spectrum_to_file(new_fname,spec,filt_dict)
+           z,tab = add_spectrum_to_file(new_fname,spec,sn,filt_dict)
         except RuntimeError:
             continue
+    print(sn)
+    continue
+    mod = sncosmo.Model('salt3')
+    mod.set(z=z)
+    lc = get_filter_info('kcor_uvext.fits',tab,n)
+    print(len(lc),len(tab))
+    
+    for filt in np.unique(lc['band']):
+        b = sncosmo.get_bandpass(filt)
+        if b.wave_eff<3600 or b.wave_eff>7500 or ('ztf' not in filt.lower() and sn in ['2022eyw']):
+            lc = lc[lc['band']!=filt]
+    maxtime = lc['mjd'][np.array(lc['flux']).argmax()]
+    if sn in ['2011fe','2022eyw']:
+        lc = lc[np.where(np.logical_and(lc['mjd']-maxtime>-25,lc['mjd']-maxtime<20))[0]]
+    else:
+        lc = lc[np.where(np.logical_and(lc['mjd']-maxtime>-25,lc['mjd']-maxtime<40))[0]]
+    print(lc)
+    try:
+        
+        res,fit = sncosmo.fit_lc(lc,mod,['x0','x1','t0','c'],bounds = {'c':(-1,1),'x1':(-3,3),'x0':(0,10),'t0':np.array([-30,30])+maxtime})
+        
+        #res,fit = sncosmo.nest_lc(lc,mod,['x0','x1','t0','c'],bounds = {'c':(-1,1),'x1':(-3,3),'x0':(0,1),'t0':np.array([-30,30])+maxtime},maxiter=1000)
+        sncosmo.plot_lc(lc,fit)
+        plt.savefig(sn+'.png')
+        plt.close()
+    except:
+        sncosmo.plot_lc(lc)
+        plt.savefig(sn+'_fail.png')
+        plt.close()
+        print ('FITTING %s FAILED'%sn)
+        continue
+    filelist.append(os.path.basename(new_fname)+'\n')
+    par_file.append('%s %f %.8f %.8f %.8f 1\n'%(sn,z,fit.get('x0'),fit.get('x1'),fit.get('c')))
+    mjd_file.append('%s %.3f\n'%(sn,fit.get('t0')))
+with open('SALT3TRAIN_UVEXT.LIST','w') as list_file:
+    list_file.write(''.join(filelist))
+with open('fitpars.txt','w') as f:
+    f.write(''.join(par_file))
+with open('mjds.txt','w') as f:
+    f.write(''.join(mjd_file))
+    
         
         
-            
-   
